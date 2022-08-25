8c92d491a1cf00d32cef4343b2c6454fa61820bf
Reitumetse <joycreamoeletsi@gmail.com>
2022-08-25 23:06:31 +0200

    update

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -11,15 +11,13 @@ size_t binary_tree_depth2(const binary_tree_t *tree);
 
 size_t binary_tree_depth2(const binary_tree_t *tree)
 {
-  int cont1 = 0;
-
-  if (tree == NULL)
-    return (0);
-
-  if (tree->parent != NULL)
-    cont1 += binary_tree_depth2(tree->parent);
-
-  return (cont1 + 1);
+	int cont1 = 0;
+	
+	if (tree == NULL)
+		return (0);
+	if (tree->parent != NULL)
+		cont1 += binary_tree_depth2(tree->parent);
+	return (cont1 + 1);
 }
 
 /**
@@ -32,8 +30,7 @@ size_t binary_tree_depth2(const binary_tree_t *tree)
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-  if (tree == NULL)
-    return (0);
-
-  return (binary_tree_depth2(tree) - 1);
+	if (tree == NULL)
+		return (0);
+	return (binary_tree_depth2(tree) - 1);
 }
